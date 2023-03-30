c67c0ec436e7bc1036fcf20a548698c20f69e3de
Federico Maggi <federico.maggi@mia-platform.eu>
2023-03-30 18:03:30 +0200

    attempt to fix the loop

--- algorithm1.go
+++ algorithm1.go
@@ -1,6 +1,9 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func algorithm(
 	config *Config,
@@ -24,68 +27,83 @@ func algorithm(
 	selectedTag := config.listTags[0]
 	oldSelectedTags := selectedTag
 	for {
-		oldOldSelectedTags := []*Tags{}
-		for _, listTag := range config.listTags {
-			if len(listTag.photos) != 0 {
-				oldOldSelectedTags = append(config.listTags, listTag)
-			}
-		}
-		config.listTags = oldOldSelectedTags
 		if len(config.listTags) == 0 {
 			break
 		}
-		for _, photo := range selectedTag.photos {
-			if usedPhotos[photo.ID] {
+
+		selectedPhoto := selectedTag.photos[0]
+		fmt.Printf("SELECTED PHOTO %s - %d\n", selectedTag.name, selectedPhoto.ID)
+		if len(selectedTag.photos) > 1 {
+			selectedTag.photos = selectedTag.photos[1:]
+		} else if len(selectedTag.photos) == 1 {
+			selectedTag.photos = []*Photo{selectedTag.photos[0]}
+		} else {
+			newConfigListLtags := []*Tags{}
+			for _, listTag := range config.listTags {
+				if len(listTag.photos) != 0 {
+					newConfigListLtags = append(config.listTags, listTag)
+				}
+			}
+			config.listTags = newConfigListLtags
+			selectedTag = config.listTags[0]
+		}
+
+		// for _, photo := range selectedTag.photos {
+		if usedPhotos[selectedPhoto.ID] {
+			continue
+		}
+		usedPhotos[selectedPhoto.ID] = true
+
+		maxScoredTag := ""
+		for _, photoTag := range selectedPhoto.Tags {
+			if photoTag == selectedTag.name {
 				continue
 			}
-			usedPhotos[photo.ID] = true
-			maxScoredTag := ""
-			for _, photoTag := range photo.Tags {
-				if photoTag == selectedTag.name {
-					continue
-				}
-				if maxScoredTag == "" || config.tagsScores[photoTag] > config.tagsScores[maxScoredTag] {
-					selectedTag = &Tags{
-						name:   photoTag,
-						photos: config.tagsMap[photoTag],
-					}
-				}
-			}
-			tags := []string{}
-			for _, photoTag := range photo.Tags {
-				if photoTag != oldSelectedTags.name {
-					tags = append(tags, photoTag)
-				}
-			}
-			photo.Tags = tags
+			fmt.Printf("SHOULD SELECTED NEW ONE", photoTag)
 
-			photos := []*Photo{}
-			for _, selectedPhoto := range oldSelectedTags.photos {
-				if photo.ID != selectedPhoto.ID {
-					photos = append(photos, photo)
+			if maxScoredTag == "" || config.tagsScores[photoTag] > config.tagsScores[maxScoredTag] {
+				fmt.Printf("NEW TAG SELECTED", photoTag)
+				selectedTag = &Tags{
+					name:   photoTag,
+					photos: config.tagsMap[photoTag],
 				}
-			}
-			oldSelectedTags.photos = photos
-
-			oldSelectedTags = selectedTag
-
-			if photo.Layout == "H" {
-				slideShow.slides = append(slideShow.slides, []int{photo.ID})
-				slideIndex++
-			} else {
-				if partialSlideIndex < 0 {
-					newslide := []int{photo.ID}
-					slideShow.slides = append(slideShow.slides, newslide)
-					partialSlideIndex = slideIndex
-					slideIndex++
-					continue
-				}
-
-				// fmt.Printf("FOOOO %v %+v %d", partialSlideIndex, slideShow.slides, slideIndex)
-				slideShow.slides[partialSlideIndex] = append(slideShow.slides[partialSlideIndex], photo.ID)
-				partialSlideIndex = -1
 			}
 		}
+		tags := []string{}
+		for _, photoTag := range selectedPhoto.Tags {
+			if photoTag != oldSelectedTags.name {
+				tags = append(tags, photoTag)
+			}
+		}
+		selectedPhoto.Tags = tags
+
+		photos := []*Photo{}
+		for _, selectedPhoto := range oldSelectedTags.photos {
+			if selectedPhoto.ID != selectedPhoto.ID {
+				photos = append(photos, selectedPhoto)
+			}
+		}
+		oldSelectedTags.photos = photos
+
+		oldSelectedTags = selectedTag
+
+		if selectedPhoto.Layout == "H" {
+			slideShow.slides = append(slideShow.slides, []int{selectedPhoto.ID})
+			slideIndex++
+		} else {
+			if partialSlideIndex < 0 {
+				newslide := []int{selectedPhoto.ID}
+				slideShow.slides = append(slideShow.slides, newslide)
+				partialSlideIndex = slideIndex
+				slideIndex++
+				continue
+			}
+
+			// fmt.Printf("FOOOO %v %+v %d", partialSlideIndex, slideShow.slides, slideIndex)
+			slideShow.slides[partialSlideIndex] = append(slideShow.slides[partialSlideIndex], selectedPhoto.ID)
+			partialSlideIndex = -1
+		}
+		// }
 	}
 
 	return &slideShow
